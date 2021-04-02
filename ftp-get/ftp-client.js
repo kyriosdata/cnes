91f0f9c07f34065a63e01526f0ade99177033a61
kyriosdata <fabio@inf.ufg.br>
2021-04-02 13:25:24 -0300

    doc

--- ftp-get/ftp-client.js
+++ ftp-get/ftp-client.js
@@ -16,8 +16,11 @@ class FtpClient {
    * meio do construtor ou ser fornecido a cada
    * arquivo a ser baixado.
    *
-   * @param {string} server Servidor de ftp. Se não fornecido é assumido 'ftp.datasus.gov.br'.
+   * Após a instância não ser mais necessária ela deve
+   * ser fechada (método 'close').
+   *
    * @param {boolean} progressBar Indica se barra de progressão (console) deve ser exibida (padrão) ou não.
+   * @param {string} server Servidor de ftp. Se não fornecido é assumido 'ftp.datasus.gov.br'.
    */
   constructor(progressBar = true, server = FTP_SERVER) {
     this.client = new Client();
@@ -29,6 +32,13 @@ class FtpClient {
     this.progressBar = !!exibir;
   }
 
+  /**
+   * Obtém o tamanho em bytes do arquivo indicado e disponível no servidor de FTP.
+   * @param {string} file Nome do arquivo a ser obtido.
+   * @param {string} dir Diretório a partir da raiz do servidor FTP onde se encontra o arquivo.
+   * @param {string} server Servidor (host) para o qual a conexão será estabelecida.
+   * @returns Promise<number> Promise com a indicação da quantidade de bytes do arquivo.
+   */
   size(file, dir = "", server = this.server) {
     return new Promise((resolve, reject) => {
       this.client.on("ready", () => {
@@ -45,6 +55,19 @@ class FtpClient {
     });
   }
 
+  /**
+   * Obtém arquivo de servidor de FTP. O arquivo obtido é persistido
+   * no destino indicado (se não fornecido, então no diretório corrente será
+   * criado arquivo de mesmo nome daquele baixado).
+   *
+   * Se não for fornecido um diretório, então assume-se que o arquivo encontra-se na raiz do servidor de FTP.
+   *
+   * @param {string} file Nome do arquivo a ser baixado.
+   * @param {string} dir Diretório no qual se encontro o arquivo a ser baixado.
+   * @param {string} destino Local de destino do arquivo obtido. Se não fornecido, o nome do arquivo baixado será empregado no diretório corrente.
+   * @param {string} server Servidor de FTP do qual o arquivo será baixado.
+   * @returns Promise<number> indicando a quantidade de bytes obtidos.
+   */
   async get(file, dir = "", destino = file, server = this.server) {
     const totalBytes = await this.size(file, dir, server);
 
