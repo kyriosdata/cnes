28812451e95e83a4eb90f819598f6924a243e68e
kyriosdata <fabio@inf.ufg.br>
2021-04-02 13:32:46 -0300

    doc

--- ftp-get/cnes-download.js
+++ ftp-get/cnes-download.js
@@ -6,7 +6,7 @@ const FTP_FILE = "BASE_DE_DADOS_CNES_202102.ZIP";
 async function main() {
   try {
     const cliente = new FtpClient();
-    const lidos = await cliente.get(FTP_FILE, FTP_DIR);
+    const lidos = await cliente.size(FTP_FILE, FTP_DIR);
     cliente.close();
     console.log(lidos);
   } catch (erro) {
